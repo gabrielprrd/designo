14344d4e00cb56029e8df3f8bfafb4f3128ca660
Gabriel Afonso <gabriel.afonso.contato@hotmail.com>
2023-06-13 19:17:45 +0100

    feat: add jost font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,10 @@ import type { AppProps } from "next/app";
 import Head from "next/head";
 import type { NextPage } from "next";
 import type { ReactElement, ReactNode } from "react";
+import { Jost } from "next/font/google";
+import "../styles/globals.css";
+
+const jost = Jost({ subsets: ["latin"] });
 
 // eslint-disable-next-line @typescript-eslint/ban-types
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
@@ -19,6 +23,11 @@ function App({ Component, pageProps: { ...pageProps } }: AppPropsWithLayout) {
       <Head>
         <link rel="shortcut icon" href="/favicon.png" />
       </Head>
+      <style jsx global>{`
+        html {
+          font-family: ${jost.style.fontFamily};
+        }
+      `}</style>
       <main>{getLayout(<Component {...pageProps} />)}</main>
     </>
   );
