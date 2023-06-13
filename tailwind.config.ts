8a2e32209706ed0970b7d3759edcb57d553c6167
Gabriel Afonso <gabriel.afonso.contato@hotmail.com>
2023-06-13 19:16:03 +0100

    feat: add tailwind globals and initial custom theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,17 @@
 import { type Config } from "tailwindcss";
 
 export default {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "light-peach": "#FFAD9B",
+        peach: "#E7816B",
+        black: "#1D1C1E",
+        "dark-grey": "#333136",
+        "light-grey": "#F1F3F5",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("@headlessui/tailwindcss")],
 } satisfies Config;
