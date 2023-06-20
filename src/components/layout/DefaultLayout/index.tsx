ee77b9f4df53dc090cabd463ff59dee8c76d71b2
Gabriel Afonso <gabriel.afonso.contato@hotmail.com>
2023-06-20 12:24:00 +0100

    feat: add background image to defaultLayout

--- src/components/layout/DefaultLayout/index.tsx
+++ src/components/layout/DefaultLayout/index.tsx
@@ -1,6 +1,7 @@
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
 import type { ReactElement } from "react";
+import FooterCallToAction from "@/components/layout/FooterCallToAction";
 
 interface Props {
   children: ReactElement;
@@ -11,10 +12,12 @@ export default function DefaultLayout({ children }: Props) {
     <div className="flex w-screen flex-col">
       <div className="min-h-screen">
         <Header />
-        <main className="">{children}</main>
+        <main className="bg-no-repeat lg:bg-[url('/assets/shared/desktop/bg-pattern-leaf.svg'),_url('/assets/shared/desktop/bg-pattern-leaf.svg')] lg:bg-[position:left_top_10vh,_right_bottom]">
+          {children}
+        </main>
       </div>
 
-      <Footer />
+      <Footer ctaCard={<FooterCallToAction />} />
     </div>
   );
 }
