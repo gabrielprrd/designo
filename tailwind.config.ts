247e603db903c2445e4c40ac5721c3b43c0ae11f
Gabriel Afonso <gabriel.afonso.contato@hotmail.com>
2023-06-20 12:33:19 +0100

    chore: add necessary image paths at tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,23 @@ export default {
         "dark-grey": "#333136",
         "light-grey": "#F1F3F5",
       },
+      backgroundImage: {
+        "hero-pattern-home":
+          "url('/assets/home/desktop/bg-pattern-hero-home.svg')",
+        "pattern-call-to-action":
+          "url('/assets/shared/desktop/bg-pattern-call-to-action.svg')",
+        "pattern-leaf": "url('/assets/shared/desktop/bg-pattern-leaf.svg')",
+      },
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+      },
+      letterSpacing: {
+        widest: ".3em",
+      },
     },
   },
   plugins: [require("@headlessui/tailwindcss")],
