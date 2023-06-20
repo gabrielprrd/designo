2ea3e0cbca5d3054ae7482adc1a282cbc274676f
Gabriel Afonso <gabriel.afonso.contato@hotmail.com>
2023-06-20 12:27:21 +0100

    feat: add PROJECTS constant

---
+++ src/constants/projects.ts
@@ -0,0 +1,29 @@
+export const PROJECTS = [
+  {
+    title: "Web Design",
+    cta: "View projects",
+    srcMobile: "/assets/home/mobile/image-web-design.jpg",
+    srcTablet: "/assets/home/tablet/image-web-design.jpg",
+    srcDesktop: "/assets/home/desktop/image-web-design-large.jpg",
+    alt: "Hand typing on a notebook",
+    href: "/web-design",
+  },
+  {
+    title: "App Design",
+    cta: "View projects",
+    srcMobile: "/assets/home/mobile/image-app-design.jpg",
+    srcTablet: "/assets/home/tablet/image-app-design.jpg",
+    srcDesktop: "/assets/home/desktop/image-app-design.jpg",
+    alt: "Hand carrying a smartphone",
+    href: "/app-design",
+  },
+  {
+    title: "Graphic Design",
+    cta: "View projects",
+    srcMobile: "/assets/home/mobile/image-graphic-design.jpg",
+    srcTablet: "/assets/home/tablet/image-graphic-design.jpg",
+    srcDesktop: "/assets/home/desktop/image-graphic-design.jpg",
+    alt: "Abstract art",
+    href: "/graphic-design",
+  },
+];
