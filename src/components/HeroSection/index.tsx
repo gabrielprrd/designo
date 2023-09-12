d0966a7802af80f20a6398f34a82e4bf848a723a
Gabriel Afonso <gabriel.afonso.contato@hotmail.com>
2023-09-12 10:55:25 +0200

    refactor: remove unused class names

--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -6,7 +6,7 @@ interface Props {
 export default function HeroSection({ title, subtitle }: Props) {
   return (
     <section className="flex h-auto flex-col items-center justify-center bg-peach bg-hero-pattern-home bg-[length:80%_auto] bg-right-top bg-no-repeat px-4 py-20 text-center sm:mx-14 sm:rounded-lg sm:bg-pattern-call-to-action sm:bg-center sm:bg-repeat lg:mx-28 lg:px-12">
-      <div className="flex w-auto w-full flex-col items-center justify-center gap-6 text-center sm:w-[70%] lg:w-[45%]">
+      <div className="flex w-full flex-col items-center justify-center gap-6 text-center sm:w-[70%] lg:w-[45%]">
         <h1 className="text-3xl font-medium text-white sm:text-5xl">{title}</h1>
         {subtitle && <p className="text-white">{subtitle}</p>}
       </div>
